0029af6d0bd9ae6590b8663725984de84895704c
Olek Motyka <amotyka@blueform.pl>
2018-10-22 20:41:58 +0200

    Initial

---
+++ SQLServerServicesManager/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SQLServerServicesManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(260, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(10, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Service";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tray
+            // 
+            this.tray.Icon = ((System.Drawing.Icon)(resources.GetObject("tray.Icon")));
+            this.tray.Text = "MsSqlTurnOnOff";
+            this.tray.Visible = true;
+            this.tray.DoubleClick += new System.EventHandler(this.try_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(434, 111);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 500);
+            this.MinimumSize = new System.Drawing.Size(450, 150);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon tray;
+    }
+}
+
---
+++ SQLServerServicesManager/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Management;
+using MSSQLTunOnOff.Properties;
+
+namespace SQLServerServicesManager
+{
+    public partial class Form1 : Form
+    {
+        ServiceController[] services = ServiceController.GetServices().Where(x => x.ServiceName.Contains("SQL")).ToArray();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Text = Application.ProductName;
+            this.Icon = Resources.logo;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < services.Count(); i++)
+            {
+                var svc = services[i];
+                Button b = new Button();
+                b.Name = "b" + svc.ServiceName;
+                b.AutoSize = false;
+                b.Text = svc.DisplayName;
+                b.TextAlign = ContentAlignment.MiddleLeft;
+                b.Size = new Size(250, 25);
+                b.Location = new Point(5, i * 25 + 20);
+                b.Tag = svc;
+                b.Click += Button_Click;
+                Controls.Add(b);
+                b.Show();
+
+                Label l = new Label();
+                l.Text = "";
+                l.Name = "l" + svc.ServiceName;
+                l.Tag = svc;
+                l.TextAlign = ContentAlignment.MiddleLeft;
+                l.Location = new Point(260, b.Location.Y);
+                l.AutoSize = false;
+                l.Size = new Size(50, 25);
+                Controls.Add(l);
+                l.Show();
+
+                updateSvcInfo(svc);
+            }
+            Size = MinimumSize = MaximumSize = new Size(330, services.Count() * 25 + 70);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            var svc = btn.Tag as ServiceController;
+            var lbl = Controls["l" + svc.ServiceName] as Label;
+            try
+            {
+                if (svc.Status == ServiceControllerStatus.Stopped || svc.Status == ServiceControllerStatus.Paused)
+                {
+                    svc.Start();
+                    svc.WaitForStatus(ServiceControllerStatus.StartPending);
+                    updateSvcInfo(svc);
+                    svc.WaitForStatus(ServiceControllerStatus.Running);
+                    updateSvcInfo(svc);
+                }
+                else if (svc.Status == ServiceControllerStatus.Running)
+                {
+                    svc.Stop();
+                    svc.WaitForStatus(ServiceControllerStatus.Stopped);
+                    updateSvcInfo(svc);
+                }
+            }
+            catch { }
+        }
+
+        private void updateSvcInfo(ServiceController svc)
+        {
+            var lbl = (Controls["l" + svc.ServiceName] as Label);
+            var btn = (Controls["b" + svc.ServiceName] as Button);
+            switch (svc.Status)
+            {
+                case ServiceControllerStatus.Running:
+                    ManagementObject service = new ManagementObject(@"Win32_service.Name='" + svc.ServiceName + "'");
+                    var id = (uint)service.GetPropertyValue("ProcessId");
+                    lbl.Text = id.ToString();
+                    btn.BackColor = Color.LightGreen;
+                    break;
+                case ServiceControllerStatus.Stopped:
+                    lbl.Text = "";
+                    btn.BackColor = Color.IndianRed;
+                    break;
+                case ServiceControllerStatus.StartPending:
+                    lbl.Text = "";
+                    btn.BackColor = Color.LightYellow;
+                    break;
+                case ServiceControllerStatus.StopPending:
+                    lbl.Text = "";
+                    btn.BackColor = Color.LightYellow;
+                    break;
+                default:
+                    lbl.Text = "";
+                    btn.BackColor = Color.White;
+                    break;
+            }
+        }
+
+        private string RunCmd(string command)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = command.Split(' ')[0];
+            p.StartInfo.Arguments = command.Substring(command.IndexOf(' ') + 1);
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+            return p.StandardOutput.ReadToEnd();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach(var s in services)
+            {
+                if (s.Status == ServiceControllerStatus.Running)
+                {
+                    s.Stop();
+                    s.WaitForStatus(ServiceControllerStatus.StopPending);
+                    updateSvcInfo(s);
+                    s.WaitForStatus(ServiceControllerStatus.Stopped);
+                    updateSvcInfo(s);
+                }
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            ShowInTaskbar = WindowState != FormWindowState.Minimized;
+        }
+
+        private void try_DoubleClick(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+                BringToFront();
+            }
+            else
+            {
+                WindowState = FormWindowState.Minimized;
+            }
+        }
+    }
+}
